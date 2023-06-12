dccae7316958c5e504c3ee62343eb2746f4c88f1
artur <artur@LAPTOP-7QF1CQ81>
2023-06-12 13:17:45 +0300

    PostRepository update

--- src/main/java/telran/java47/post/dao/PostRepository.java
+++ src/main/java/telran/java47/post/dao/PostRepository.java
@@ -21,15 +21,6 @@ public interface PostRepository  extends MongoRepository<Post, String> {
 	
 	Stream<Post> findPostsByTags(List<String> tags);
 	
-//	@Query("{'scores.?0' : {'$gte': ?1}}")
-
-//	dateCreated: {
-//        $gt: ISODate("2020-01-21"),$lt: ISODate("2024-01-24")
- //   }}
-	@Query( value =  "{dateCreated:{'$gte': ISODate(?0) ,'$lt' : ISODate(?1)}}")
-//	@Query(value = "Select t from Post t where dateCreated BETWEEN :startDate AND :endDate")
-	//public List<EntityClassTable> getAllBetweenDates(@Param("startDate")Date startDate,@Param("endDate")Date endDate);
-	Stream<Post> findPostsByPeriod(@Param("startDate")LocalDate startDate,@Param("endDate") LocalDate endDate);
-	
-	//Stream<Post> findPostsByPeriod(LocalDate startDate,LocalDate endDate);
+	@Query("{'dateCreated' : { $gte: ?0, $lte: ?1 } }")  
+	Stream<Post> findPostsByPeriod(LocalDate startDate,LocalDate endDate);
 }
